87de3c31664cfcd84c4d9b9a4ae91c5c4f973287
Srikanth Cheruku <wpsrikanth@gmail.com>
2023-10-18 19:14:28 +0530

    final

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,14 +4,15 @@ const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) return res.sendStatus(401);
+  if (!username || !password)
+    return res.status(400).json({ message: "username or password missing!" });
 
   const duplicate = await User.findOne({ username: username }).exec();
   if (duplicate) return res.sendStatus(409);
 
   const hashedPassword = await bcrypt.hash(password, 12);
   const result = await User.create({ username, password: hashedPassword });
-  
+
   console.log(result);
 
   res.sendStatus(201);
@@ -41,8 +42,13 @@ const handleLogin = async (req, res) => {
     );
     findUser.refreshToken = refreshToken;
     const result = await findUser.save();
-    console.log(result, 'loggedin');
-    res.cookie("jwt", refreshToken, { httpOnly: true, secure: true, sameSite: "None", maxAge: 86400000});
+    console.log(result, "loggedin");
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 86400000,
+    });
     return res.status(200).json({ accessToken });
   } else {
     res.status(401).json({ message: "login failed" });
--- server.js
+++ server.js
@@ -5,11 +5,29 @@ const verifyJWT = require("./middleware/verifyJWT");
 require("dotenv").config();
 const connectDB = require("./config/dbConfig");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 
 connectDB();
+const allowlist = ["http://localhost:3000", "https://expensemanage.vercel.app"];
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "expensemanage.vercel.app/login");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
+app.use(
+  cors({
+    credentials: true,
+    origin: allowlist,
+  })
+);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +40,6 @@ app.use(verifyJWT);
 app.use("/users", require("./routes/user"));
 app.use("/api", require("./routes/api/transaction"));
 app.use("/api", require("./routes/api/debt"));
-
 
 const PORT = process.env.port || 3500;
 

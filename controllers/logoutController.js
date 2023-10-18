dd38e18478acebd1d11a9c1e2462fe65a8b869ac
Srikanth Cheruku <wpsrikanth@gmail.com>
2023-10-18 20:29:30 +0530

    error handling in logout

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -7,17 +7,22 @@ const handleLogout = async (req, res) => {
   const refreshToken = cookies.jwt;
 
   const foundUser = await User.findOne({ refreshToken }).exec();
+
   if (!foundUser) {
     res.clearCookie("jwt", { httpOnly: true });
     res.sendStatus(204);
   }
-
-  foundUser.refreshToken = '';
-  const result = await foundUser.save()
-  console.log(result)
-
-  res.clearCookie("jwt", { httpOnly: true });
-  res.sendStatus(204);
+  try {
+    foundUser?.refreshToken = '';
+    const result = await foundUser.save()
+    console.log(result)
+  
+    res.clearCookie("jwt", { httpOnly: true });
+    return res.sendStatus(204);
+  }catch(e) {
+   return res.sendStatus(400)
+  }
+  
 };
 
 
